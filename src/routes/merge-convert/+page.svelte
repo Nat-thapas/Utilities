5dc8b4eb63557e7c9ce0d46d8e4faaf4f0d6db93
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2024-03-30 19:36:41 +0700

    Add file reordering

--- src/routes/merge-convert/+page.svelte
+++ src/routes/merge-convert/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	// @hmr:keep-all
+
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
@@ -9,6 +11,17 @@
 	import LoaderCircle from 'lucide-svelte/icons/loader-circle';
 	import { PDFDocument } from 'pdf-lib';
 	import { toast } from 'svelte-sonner';
+	import SortableList from '$lib/components/ui/SortableList.svelte';
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
+	import ChevronsUpDown from 'lucide-svelte/icons/chevrons-up-down';
+	import ArrowUp from 'lucide-svelte/icons/arrow-up';
+	import ArrowDown from 'lucide-svelte/icons/arrow-down';
+	import ArrowUpToLine from 'lucide-svelte/icons/arrow-up-to-line';
+	import ArrowDownToLine from 'lucide-svelte/icons/arrow-down-to-line';
+	import Trash2 from 'lucide-svelte/icons/trash-2';
+	import EllipsisVertical from 'lucide-svelte/icons/ellipsis-vertical';
 
 	const scaleTypes = [
 		{ value: 'all', label: 'All pages' },
@@ -156,16 +169,69 @@
 	let processing = false;
 	let downloadUrl: string = '';
 
+	let filesPreview: Record<any, any>[] = [];
+
+	function updateFilesPreview() {
+		URL.revokeObjectURL(downloadUrl);
+		downloadUrl = '';
+		progressPrecent = 0;
+		filesPreview = [];
+		if (files?.length) {
+			for (let i = 0; i < files.length; i++) {
+				filesPreview.push({
+					id: i.toString(),
+					name: files[i].name,
+					type: files[i].type
+				});
+			}
+		}
+	}
+
+	let filesPreviewComponent: any;
+
+	function sortFilesPreviewOnDrag(evnt: CustomEvent) {
+		filesPreview = evnt.detail;
+	}
+
+	function sortFilesPreviewOnClick(evnt: CustomEvent) {
+		// @ts-ignore
+		const from = evnt.detail.currentTarget.dataset.from;
+		// @ts-ignore
+		const to = evnt.detail.currentTarget.dataset.to;
+		filesPreviewComponent.reorder({ from, to });
+	}
+
+	function removeFilePreviewOnClick(evnt: CustomEvent) {
+		// @ts-ignore
+		const index = parseInt(evnt.detail.currentTarget.dataset.index);
+		if (index < 0) return;
+		if (index >= filesPreview.length) return;
+		const newFilesPreview = [...filesPreview];
+		newFilesPreview.splice(index, 1);
+		filesPreview = newFilesPreview;
+	}
+
 	async function mergeFiles() {
 		processing = true;
 		URL.revokeObjectURL(downloadUrl);
 		downloadUrl = '';
+		progressPrecent = 0;
+
+		if (!files?.length) {
+			toast.error('No files selected', {
+				description: 'Please select at least one file to merge'
+			});
+			processing = false;
+			return;
+		}
 
 		const mergedPDF = await PDFDocument.create();
 
-		progressPrecent = 0;
+		let fileNumber = 0;
 
-		for (let fileIndex = 0; fileIndex < files.length; fileIndex++) {
+		for (let filePreviewData of filesPreview) {
+			const fileIndex = parseInt(filePreviewData.id);
+
 			const progressPrecentPerFile = 80 / files.length;
 
 			const file = files[fileIndex];
@@ -268,7 +334,7 @@
 							}
 							mergedPDF.addPage(page);
 							progressPrecent =
-								fileIndex * progressPrecentPerFile +
+								fileNumber * progressPrecentPerFile +
 								((pageIndex + 1) * progressPrecentPerFile) / pageCount;
 						}
 					} catch (error) {
@@ -288,7 +354,9 @@
 					break;
 			}
 
-			progressPrecent = (fileIndex + 1) * progressPrecentPerFile;
+			progressPrecent = (fileNumber + 1) * progressPrecentPerFile;
+
+			fileNumber++;
 		}
 
 		if (mergedPDF.getPageCount() === 0) {
@@ -314,124 +382,195 @@
 		progressPrecent = 100;
 		processing = false;
 	}
+
+	$: smallScreen = innerWidth < 640;
+	$: largeScreen = innerWidth >= 640;
 </script>
 
 <svelte:head>
 	<title>Merge/Convert to PDF</title>
 </svelte:head>
 
-<div class="grid w-[22rem] items-center gap-1.5 mx-auto mt-4 px-4">
-	<h1 class="text-xl font-semibold">Merge/Convert</h1>
-	<hr />
-	<h2 class="mb-1">Merge PDF or image files into one PDF file</h2>
-	<Label for="input-files">Input files</Label>
-	<input
-		id="input-files"
-		class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-		type="file"
-		accept="application/pdf,image/png,image/jpg,image/jpeg"
-		multiple
-		bind:files
-	/>
-
-	<div class="flex justify-between">
-		<div>
-			<Label for="scale-type">Scale</Label>
-			<Select.Root bind:selected={scaleType}>
-				<Select.Trigger class="w-36">
-					<Select.Value placeholder="Scale type" />
+<div class="flex flex-wrap w-full justify-center">
+	<div class="grid w-[22rem] h-fit items-center gap-1.5 mt-4 px-4">
+		<h1 class="text-xl font-semibold">Merge/Convert</h1>
+		<hr />
+		<h2 class="mb-1">Merge PDF or image files into one PDF file</h2>
+		<Label for="input-files">Input files</Label>
+		<input
+			id="input-files"
+			class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+			type="file"
+			accept="application/pdf,image/png,image/jpg,image/jpeg"
+			multiple
+			bind:files
+			on:change={updateFilesPreview}
+		/>
+		<div class="flex justify-between">
+			<div>
+				<Label for="scale-type">Scale</Label>
+				<Select.Root bind:selected={scaleType}>
+					<Select.Trigger class="w-36">
+						<Select.Value placeholder="Scale type" />
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.Label>Scale type</Select.Label>
+							{#each scaleTypes as type}
+								<Select.Item value={type.value} label={type.label}>{type.label}</Select.Item>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+					<Select.Input name="scale-type" />
+				</Select.Root>
+			</div>
+			<div>
+				<Label for="scale-dimension">Dimension</Label>
+				<Select.Root bind:selected={scaleDimension}>
+					<Select.Trigger class="w-40">
+						<Select.Value placeholder="Scale dimension" />
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.Label>Scale dimension</Select.Label>
+							{#each scaleDimensions as dimension}
+								<Select.Item value={dimension.value} label={dimension.label}
+									>{dimension.label}</Select.Item
+								>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+					<Select.Input name="scale-dimension" />
+				</Select.Root>
+			</div>
+		</div>
+		<Label for="output-size">Output {scaleDimension.label.toLowerCase()}</Label>
+		<div class="flex">
+			<div
+				class="flex h-10 mr-2 rounded-md border border-input bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+			>
+				<Input
+					type="number"
+					id="output-size"
+					class="border-0 h-[2.375rem] mr-1"
+					placeholder="Output size"
+					bind:value={outputSize}
+					on:change={removePresetSize}
+				/>
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger asChild let:builder>
+						<Button variant="secondary" class="h-[2.375rem]" builders={[builder]}>Presets</Button>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content class="w-32">
+						<DropdownMenu.Label>Preset sizes</DropdownMenu.Label>
+						<DropdownMenu.Separator />
+						<DropdownMenu.RadioGroup bind:value={selectedPresetSize}>
+							{#each [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as paperSize}
+								<DropdownMenu.RadioItem value="a{paperSize}">A{paperSize}</DropdownMenu.RadioItem>
+							{/each}
+							<DropdownMenu.RadioItem value="letter">Letter</DropdownMenu.RadioItem>
+							<DropdownMenu.RadioItem value="legal">Legal</DropdownMenu.RadioItem>
+						</DropdownMenu.RadioGroup>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			</div>
+			<Select.Root bind:selected={outputSizeUnit}>
+				<Select.Trigger class="w-48">
+					<Select.Value placeholder="Unit" />
 				</Select.Trigger>
 				<Select.Content>
 					<Select.Group>
-						<Select.Label>Scale type</Select.Label>
-						{#each scaleTypes as type}
-							<Select.Item value={type.value} label={type.label}>{type.label}</Select.Item>
+						<Select.Label>Unit</Select.Label>
+						{#each outputSizeUnits as unit}
+							<Select.Item value={unit.value} label={unit.label}>{unit.label}</Select.Item>
 						{/each}
 					</Select.Group>
 				</Select.Content>
-				<Select.Input name="scale-type" />
+				<Select.Input name="scale-unit" />
 			</Select.Root>
 		</div>
-		<div>
-			<Label for="scale-dimension">Dimension</Label>
-			<Select.Root bind:selected={scaleDimension}>
-				<Select.Trigger class="w-40">
-					<Select.Value placeholder="Scale dimension" />
-				</Select.Trigger>
-				<Select.Content>
-					<Select.Group>
-						<Select.Label>Scale dimension</Select.Label>
-						{#each scaleDimensions as dimension}
-							<Select.Item value={dimension.value} label={dimension.label}
-								>{dimension.label}</Select.Item
-							>
-						{/each}
-					</Select.Group>
-				</Select.Content>
-				<Select.Input name="scale-dimension" />
-			</Select.Root>
+		<Progress class="mt-1 mb-0.5" value={progressPrecent} />
+		{#if !processing}
+			<Button on:click={mergeFiles}>Merge and Download</Button>
+		{:else}
+			<Button disabled>
+				<LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+				Processing: {progressPrecent.toFixed(0)}%
+			</Button>
+		{/if}
+		{#if downloadUrl}
+			<a href={downloadUrl} class="w-full h-fit" download="output.pdf">
+				<Button variant="secondary" class="w-full"><Download class="mr-2 h-4 w-4" />Download</Button
+				>
+			</a>
+		{/if}
+	</div>
+	<div class="w-80 mx-4 mt-4">
+		<Label class="text-base">Selected files</Label>
+		<div class="w-full h-fit rounded-md border mt-2">
+			<SortableList
+				bind:this={filesPreviewComponent}
+				list={filesPreview}
+				on:sort={sortFilesPreviewOnDrag}
+				let:item
+				let:index
+			>
+				<div class="flex items-center hover:bg-muted hover:bg-opacity-40 rounded-lg p-1.5 m-0.5">
+					<Avatar.Root class="w-8 h-8">
+						<Avatar.Fallback class="font-semibold">{index + 1}</Avatar.Fallback>
+					</Avatar.Root>
+					<p class="overflow-hidden text-nowrap text-ellipsis w-52 ml-2">
+						{item.name}
+					</p>
+					<DropdownMenu.Root>
+						<DropdownMenu.Trigger>
+							<Button variant="ghost" size="sm" class="w-9 p-0 ml-2">
+								<EllipsisVertical class="h-4 w-4" />
+							</Button>
+						</DropdownMenu.Trigger>
+						<DropdownMenu.Content>
+							<DropdownMenu.Group>
+								<DropdownMenu.Label>Modify</DropdownMenu.Label>
+								<DropdownMenu.Separator />
+								<DropdownMenu.Item
+									class="cursor-pointer"
+									data-from={index}
+									data-to={index - 1}
+									on:click={sortFilesPreviewOnClick}
+									><ArrowUp class="mr-2 h-4 w-4" />Move up</DropdownMenu.Item
+								>
+								<DropdownMenu.Item
+									class="cursor-pointer"
+									data-from={index}
+									data-to={index + 1}
+									on:click={sortFilesPreviewOnClick}
+									><ArrowDown class="mr-2 h-4 w-4" />Move down</DropdownMenu.Item
+								>
+								<DropdownMenu.Item
+									class="cursor-pointer"
+									data-from={index}
+									data-to="0"
+									on:click={sortFilesPreviewOnClick}
+									><ArrowUpToLine class="mr-2 h-4 w-4" />Move to top</DropdownMenu.Item
+								>
+								<DropdownMenu.Item
+									class="cursor-pointer"
+									data-from={index}
+									data-to={filesPreview.length - 1}
+									on:click={sortFilesPreviewOnClick}
+									><ArrowDownToLine class="mr-2 h-4 w-4" />Move to bottom</DropdownMenu.Item
+								>
+								<DropdownMenu.Item
+									class="cursor-pointer"
+									data-index={index}
+									on:click={removeFilePreviewOnClick}
+									><Trash2 class="mr-2 h-4 w-4" />Remove</DropdownMenu.Item
+								>
+							</DropdownMenu.Group>
+						</DropdownMenu.Content>
+					</DropdownMenu.Root>
+				</div>
+			</SortableList>
 		</div>
 	</div>
-
-	<Label for="output-size">Output {scaleDimension.label.toLowerCase()}</Label>
-	<div class="flex">
-		<div
-			class="flex h-10 mr-2 rounded-md border border-input bg-background text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-		>
-			<Input
-				type="number"
-				id="output-size"
-				class="border-0 h-[2.375rem] mr-1"
-				placeholder="Output size"
-				bind:value={outputSize}
-				on:change={removePresetSize}
-			/>
-			<DropdownMenu.Root>
-				<DropdownMenu.Trigger asChild let:builder>
-					<Button variant="secondary" class="h-[2.375rem]" builders={[builder]}>Presets</Button>
-				</DropdownMenu.Trigger>
-				<DropdownMenu.Content class="w-32">
-					<DropdownMenu.Label>Preset sizes</DropdownMenu.Label>
-					<DropdownMenu.Separator />
-					<DropdownMenu.RadioGroup bind:value={selectedPresetSize}>
-						{#each [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as paperSize}
-							<DropdownMenu.RadioItem value="a{paperSize}">A{paperSize}</DropdownMenu.RadioItem>
-						{/each}
-						<DropdownMenu.RadioItem value="letter">Letter</DropdownMenu.RadioItem>
-						<DropdownMenu.RadioItem value="legal">Legal</DropdownMenu.RadioItem>
-					</DropdownMenu.RadioGroup>
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
-		</div>
-		<Select.Root bind:selected={outputSizeUnit}>
-			<Select.Trigger class="w-48">
-				<Select.Value placeholder="Unit" />
-			</Select.Trigger>
-			<Select.Content>
-				<Select.Group>
-					<Select.Label>Unit</Select.Label>
-					{#each outputSizeUnits as unit}
-						<Select.Item value={unit.value} label={unit.label}>{unit.label}</Select.Item>
-					{/each}
-				</Select.Group>
-			</Select.Content>
-			<Select.Input name="scale-unit" />
-		</Select.Root>
-	</div>
-
-	<Progress class="mt-1 mb-0.5" value={progressPrecent} />
-
-	{#if !processing}
-		<Button on:click={mergeFiles}>Merge and Download</Button>
-	{:else}
-		<Button disabled>
-			<LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
-			Processing: {progressPrecent.toFixed(0)}%
-		</Button>
-	{/if}
-	{#if downloadUrl}
-		<a href={downloadUrl} class="w-full h-fit" download="output.pdf">
-			<Button variant="secondary" class="w-full"><Download class="mr-2 h-4 w-4" />Download</Button>
-		</a>
-	{/if}
 </div>
